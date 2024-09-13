80884644c76fdc1a496df8d3c2c1301c9ebf6f00
OsorioSaul <saul.osorio,hernandez@gmail.com>
2024-09-13 20:32:32 +0200

    issue#11: implementa mensaje super necesario

--- src/main/java/es/upm/miw/iwvg_devops/package1/MyFirstClass.java
+++ src/main/java/es/upm/miw/iwvg_devops/package1/MyFirstClass.java
@@ -3,5 +3,6 @@ package es.upm.miw.iwvg_devops.package1;
 public class MyFirstClass {
     public static void hola(){
         System.out.println("Hola amigo \n Esta es una segunda linea mejorada");
+        System.out.println("Mensaje super necesario");
     }
 }
