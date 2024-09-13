ffff85e6bc7341394298d96d7dbb8c424838d2d9
OsorioSaul <saul.osorio,hernandez@gmail.com>
2024-09-13 20:20:35 +0200

    issue#3: implementa segunda clase

---
+++ src/main/java/es/upm/miw/iwvg_devops/package1/MySecondClass.java
@@ -0,0 +1,8 @@
+package es.upm.miw.iwvg_devops.package1;
+
+public class MySecondClass {
+    public static void holaSegundaVersion(){
+        MyFirstClass.hola();
+        System.out.println("Llamando desde la segunda clase");
+    }
+}
