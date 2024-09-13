029137cb604b5e9355cfe53e7a485a45049cd1e7
OsorioSaul <saul.osorio,hernandez@gmail.com>
2024-09-13 20:30:22 +0200

    issue#10: implementa el metodo de adios

--- src/main/java/es/upm/miw/iwvg_devops/package1/MyFirstClass.java
+++ src/main/java/es/upm/miw/iwvg_devops/package1/MyFirstClass.java
@@ -4,4 +4,8 @@ public class MyFirstClass {
     public static void hola(){
         System.out.println("Hola amigo \n Esta es una segunda linea mejorada");
     }
+
+    public static void adios(){
+        System.out.println("¡Hasta luego!");
+    }
 }
