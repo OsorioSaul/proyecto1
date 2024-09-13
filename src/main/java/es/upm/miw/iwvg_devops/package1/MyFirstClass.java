4d7fa679f73aafb3acd003e784b4a0135e6a1b3a
OsorioSaul <saul.osorio,hernandez@gmail.com>
2024-09-13 20:10:18 +0200

    issue#2: anade segunda linea en el mensaje

--- src/main/java/es/upm/miw/iwvg_devops/package1/MyFirstClass.java
+++ src/main/java/es/upm/miw/iwvg_devops/package1/MyFirstClass.java
@@ -2,6 +2,6 @@ package es.upm.miw.iwvg_devops.package1;
 
 public class MyFirstClass {
     public static void hola(){
-        System.out.println("Hola amigo");
+        System.out.println("Hola amigo \n Esta es una segunda linea");
     }
 }
